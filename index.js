41bcd8e5b1d45c15964aac6caae1a335b0c8bc9a
Christopher Hernandez <cerberean661@gmail.com>
2019-12-02 13:42:48 -0800

    first commit and setup complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.end("Hello first project!");
+    res.send({ api: 'up and running...' })
+})
+
+server.get('/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200)
+            .json(users);
+        })
+        .catch(error => {
+            console.log('error on GET /users', error);
+            res
+                .status(500)
+                .json({ errorMessage: "error getting list of users from database" });
+        })
+})
+
+const port = 5000
+server.listen(port, () =>
+    console.log(`\n ** API running on post ${port} ** \n`)
+);
