ebabbec74911a0f9929253dc1f87f5a07a80736d
Christopher Hernandez <cerberean661@gmail.com>
2019-12-02 14:54:13 -0800

    all completed except for update()

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ server.get('/', (req, res) => {
 
 server.get('/users', (req, res) => {
     db.find()
-        .then(users => {
+        .then(user => {
             res.status(200)
-            .json(users);
+            .json(user);
         })
         .catch(error => {
             console.log('error on GET /users', error);
@@ -24,6 +24,63 @@ server.get('/users', (req, res) => {
                 .status(500)
                 .json({ errorMessage: "error getting list of users from database" });
         })
+})
+
+server.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then(user => {
+            res.status(200)
+            .json(user);
+        })
+        .catch(error => {
+            console.log('error on GET by id /users', error);
+            res
+                .status(500)
+                .json({ errorMessage: "error getting user from database" });
+        })
+})
+
+server.post('/users', (req, res) => {
+    const userData = req.body;
+
+    db.insert(userData)
+        .then(user => {
+            res.status(201)
+            .json(user)
+        })
+        .catch(error => {
+            console.log('error on POST /users', error);
+            res
+                .status(500)
+                .json({ errorMessage: "error adding the hub" });
+        })
+})
+
+server.delete('/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+        .then(removed => {
+            if(removed) {
+                res.status(200)
+                .json({ message: "user removed successfully" })
+            } else {
+                res.status(404)
+                .json({ message: "user not found" })
+            }
+        })
+        .catch(error => {
+            console.log('error on DELETE /users/:id', error);
+            res
+                .status(500)
+                .json({ errorMessage: "error removing the user" })
+        })
+})
+
+server.post('/user/:id', (req, res) => {
+    db.update()
 })
 
 const port = 5000
